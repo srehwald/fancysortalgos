4b487de9619d247c11f3a1c58c0ccd997aa2b621
Simon Rehwald <simon.rehwald@tum.de>
2019-06-05 20:17:13 +0200

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import './App.css';
 import { Diagram, Footer } from "./components/";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
-import { faGithub, faReact } from "@fortawesome/free-brands-svg-icons";
 
 const App: React.FC = () => {
   return (
